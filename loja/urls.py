7053b033a9713921060e1c02830051e38f3a38c3
andreluizsilv <andre_luiz29@hotmail.com>
2024-05-07 23:51:00 -0300

    inicial
    criando varias paginas do site

---
+++ loja/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', homepage, name='homepage'),
+    path('loja/', loja, name='loja'),
+    path('sacola/', sacola, name='sacola'),
+    path('minhaconta', minha_conta, name='minha_conta'),
+    path('login/', login, name='login'),
+    path('checkout', checkout, name='checkout'),
+
+]
---
+++ loja/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Create your views here.
+def homepage(request):
+    return render(request, 'homepage.html')
+
+
+def loja(request):
+    return render(request, 'loja.html')
+
+
+def sacola(request):
+    return render(request, 'sacola.html')
+
+
+def minha_conta(request):
+    return render(request, 'usuario/minha_conta.html')
+
+
+def login(request):
+    return render(request, 'usuario/login.html')
+
+
+def checkout(request):
+    return render(request, 'checkout.html')
