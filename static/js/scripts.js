4d4283234d8f8ffb90c54e4254cba761b61a4ee2
andreluizsilv <andre_luiz29@hotmail.com>
2024-06-15 15:19:51 -0300

    deploy
    static

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -23,3 +23,20 @@ var eleInfosPerfil = document.querySelector(".cabecalho__informacoes-perfil");
 eleCabecalhoLogin.addEventListener("click", function () {
   eleInfosPerfil.classList.toggle("cabecalho__informacoes-perfil--aberto");
 });
+ // Get the modal
+var modal = document.getElementById("avisoModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
